c65578efcefbf45e6ba5d068af7e43871318806b
Brock Herion <brockjosephherion@gmail.com>
2020-08-26 14:45:12 -0500

    Created model and manager

---
+++ api/managers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model where the email address is the unique identifier
+    and has an is_admin field to allow access to the admin app 
+    """
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_("The email must be set"))
+        if not password:
+            raise ValueError(_("The password must be set"))
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('role', 1)
+
+        if extra_fields.get('role') != 1:
+            raise ValueError('Superuser must have role of Global Admin')
+        return self.create_user(email, password, **extra_fields)
---
+++ api/models.py
@@ -0,0 +1,46 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils import timezone
+
+from .managers import CustomUserManager
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    ADMIN = 1
+    MANAGER = 2
+    EMPLOYEE = 3
+
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (MANAGER, 'Manager'),
+        (EMPLOYEE, 'Employee')
+    )
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    uid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True, default=7)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(default=timezone.now)
+    created_by = models.EmailField()
+    modified_by = models.EmailField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
