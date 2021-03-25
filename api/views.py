f95ac6faaac2c8d8056e754d35edc01cbfc7ffb0
Nikhil Kumar <nikhil811raj@gmail.com>
2021-03-26 02:51:22 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from .serializers import (
+    UserRegistrationSerializer,
+    UserLoginSerializer,
+    UserListSerializer
+)
+
+from .models import User
+
+
+class AuthUserRegistrationView(APIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = (AllowAny, )
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        valid = serializer.is_valid(raise_exception=True)
+
+        if valid:
+            serializer.save()
+            status_code = status.HTTP_201_CREATED
+
+            response = {
+                'success': True,
+                'statusCode': status_code,
+                'message': 'User successfully registered!',
+                'user': serializer.data
+            }
+
+            return Response(response, status=status_code)
